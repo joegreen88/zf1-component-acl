30a6b9857c968621e9710312c95bd8ee14aba864
Joe Green <joe.green.web@gmail.com>
2013-09-18 11:54:14 +0100

    Initial commit. Zend_Acl component added with unit test

---
+++ Tests/Zend/Acl/AclTest.php
@@ -0,0 +1,27 @@
+<?php
+namespace Tests\Zend\Validate;
+
+class AclTest extends \Tests\TestCase
+{
+    protected $classes = array(
+        '\Zend_Acl',
+        '\Zend_Acl_Exception',
+        '\Zend_Acl_Resource',
+        '\Zend_Acl_Role',
+        '\Zend_Acl_Assert_Interface',
+        '\Zend_Acl_Resource_Interface',
+        '\Zend_Acl_Role_Interface',
+        '\Zend_Acl_Role_Registry',
+        '\Zend_Acl_Role_Registry_Exception'
+    );
+
+    /**
+     * Ensure that the composer autoloader is finding the classes correctly.
+     */
+    public function testCanFindClasses()
+    {
+        foreach ($this->classes as $class) {
+            $this->assertTrue(class_exists($class) || interface_exists($class));
+        }
+    }
+}
